ccea2a5f30445b1aa00ef5cb0ef3f9e622c5bc34
Mahad Jama <99mahadjama@gmail.com>
2026-08-08 16:32:14 +0100

    updated alb sgx2

--- infra/modules/alb/main.tf
+++ infra/modules/alb/main.tf
@@ -9,7 +9,7 @@ resource "aws_security_group" "ALB_SG" {
     from_port           = 443
     to_port             = 443
     protocol            = "tcp"
-    cidr_blocks         = ["0.0.0.0/0"] #tfsec:ignore aws-ec2-no-public-ingress-sgr
+    cidr_blocks         = ["0.0.0.0/0"] #tfsec:ignore:aws-ec2-no-public-ingress-sgr
   }
 
   ingress {
